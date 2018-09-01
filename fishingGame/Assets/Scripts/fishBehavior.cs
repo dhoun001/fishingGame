a0cac46649558abf98aa61eadf293f2c8608aa76
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2018-09-01 00:43:50 -0700

    pushing semi complete fish movement and object pool(may want to categorize speed better later)

--- fishingGame/Assets/Scripts/fishBehavior.cs
+++ fishingGame/Assets/Scripts/fishBehavior.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class fishBehavior : MonoBehaviour {
 
-    [SerializeField]
-    public bool isRight;
-    public float speed;
+    private bool isRight;
+    private float speed;
+    private int cameraHeight;
 
     private Rigidbody2D fishRb;
 
     void Awake()
     {
+        cameraHeight = 9;
         fishRb = gameObject.GetComponent<Rigidbody2D>();
+        setStart();
     }
+
 	// Use this for initialization
 	void Start () {
-
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(isRight);
-        Debug.Log(speed);
 		if(isRight != true){
             fishRb.velocity = new Vector3(speed * -1, 0, 0);
         }
@@ -31,5 +32,31 @@ public class fishBehavior : MonoBehaviour {
         }
 	}
 
+    void setStart()
+    {
+        if (Random.Range(0, 2) != 0)
+        {
+            gameObject.transform.position = new Vector3(7, Random.Range(0, cameraHeight) - 4, 80);
+            isRight = false;
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(-7, Random.Range(0, cameraHeight) - 4, 80);
+            isRight = true;
+        }
+        speed = Random.Range(0.5f, 1.0f);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DeactivateSoonish());
+    }
+
+    private IEnumerator DeactivateSoonish()
+    {
+        yield return new WaitForSeconds(30f);
+        setStart();
+        gameObject.SetActive(false);
+    }
 
 }

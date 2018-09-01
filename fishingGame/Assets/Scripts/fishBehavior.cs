c8db4f5cb69a3d2cc480881167012deddd1e5ec4
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2018-08-31 21:45:06 -0700

    updated DOTween

--- fishingGame/Assets/Scripts/fishBehavior.cs
+++ fishingGame/Assets/Scripts/fishBehavior.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class fishBehavior : MonoBehaviour {
 
     [SerializeField]
-    public GameObject fish; 
+    public bool isRight;
+    public float speed;
+
+    private Rigidbody2D fishRb;
+
+    void Awake()
+    {
+        fishRb = gameObject.GetComponent<Rigidbody2D>();
+    }
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(isRight != true){
+            fishRb.MovePosition(fishRb.position * speed * Time.deltaTime);
+        }
 	}
 
 

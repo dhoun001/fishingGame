2ac2d9bedf96c05e74750271659893ccb4142a25
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2018-09-02 11:05:12 -0700

    Added timer to game

---
+++ fishingGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public int minutes;
+    public float seconds;
+    public float milliseconds;
+
+    private Text timeToDisplay;
+    private float currtime;
+    private float secondsMod;
+
+	// Use this for initialization
+	void Awake () {
+        currtime = 0;
+        timeToDisplay = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currtime += Time.deltaTime;
+        minutes = (int)currtime / 60;
+        seconds = Mathf.Round(currtime * 100f) / 100f;
+
+        if (seconds < 10){
+            timeToDisplay.text = minutes + ":" + "0" + seconds;
+        }
+        else{
+            timeToDisplay.text = minutes + ":" + seconds;
+        }
+	}
+
+}
